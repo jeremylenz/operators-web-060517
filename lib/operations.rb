fef4d5177333c818363145c0f40914dfc1d58f21
Jeremy Lenz <jeremylenz@gmail.com>
2017-05-19 00:53:42 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
+require 'pry'
 def unsafe?(speed)
-
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+
+	(speed < 40 || speed > 60) ? value_to_return =  true : value_to_return = false
+	binding.pry
+value_to_return
 end
-	
-
-
